bf2470b4b118cf24c1f193bdfd97b34ab4fe3212
Michael <michael@pixt.us>
2020-01-16 14:10:18 +0700

    concise code for medium article

--- index.js
+++ index.js
@@ -1,59 +1,49 @@
-import React, {useState} from "react";
+// Tabs.js
+
+import React, { useState } from "react";
 import { useSpring, animated as a } from "react-spring";
 import { useGesture } from "react-use-gesture";
-import { SectionTitle } from "../Content";
 import Tab from "./presentation/Tab";
-import { renderTitle } from "../../utils/renderUtils";
 import "./Tabs.css";
 
-const Tabs = ({
+const Tabs = ({ // a bunch of args I pass
         connect,
-        title,
         tabLabels,
         children,
         ...props
     }) => {
-    const [expandTabs, setExpandTabs] = useState(true);
-    const cardStyle = useSpring({
-        marginTop: expandTabs ? 0 : -60,
-        fontSize: expandTabs ? "1em" : "0em",
-        fontColor: expandTabs ? "rgb(126, 87, 194)" : "rgb(252, 250, 255)",
-        backgroundColor: expandTabs ? "rgb(252, 250, 255)" : "rgb(126, 87, 194)",
+
+    const [tabs, setTabs] = useState(true); // set tab state to default or collapsed
+
+    const tabStyle = useSpring({ // this is where react-spring does its thing
+        marginTop: tabs ? 0 : -60,
+        fontSize: tabs ? "1em" : "0em",
+        fontColor: tabs ? "rgb(126, 87, 194)" : "rgb(252, 250, 255)",
+        backgroundColor: tabs ? "rgba(252, 250, 255, 0)" : "rgb(126, 87, 194)",
         config: { mass: 1, tension: 400, friction: 30 }
     });
 
     const bind = useGesture({
-        onDrag: ({ down, velocity, distance, direction: [, y], delta: [yDelta] }) => {
+        onDrag: ({ down, direction: [, y], delta: [yDelta] }) => {
             const trigger = Math.abs(yDelta) >= 1;
             const dir = y > 0 ? -1 : 1;
-            // console.log("TOGGLE", expandTabs, "VELOCITY: ", velocity, "DISTANCE: ", distance, "DIRECTION: ", dir, "DELTA", Math.abs(yDelta))
-            if (!down && trigger) {
-                if (dir !== 0) {
-                    if (dir < 0 && (distance > 100 && velocity > 1)) {
-                        setExpandTabs(true);
-                    } else {
-                        setExpandTabs(false);
-                    }
-                }
-
-            }
+            if (!down && trigger) dir < 0 ? setTabs(true) : setTabs(false);
         }
     })
 
     return (
-        <>
         <a.div
             {...bind()}
             className="flex-container">
             <a.div
-                style={cardStyle}
+                style={tabStyle}
                 className="tab-header-container"
                 >
                 <div className="tab-header-content">
                     <SectionTitle
                         className="with-large-header"
-                        title={renderTitle(title)} />
-                    <Tab className={ expandTabs ? "defaultTabs" : "collapsedTabs" } connect={connect}>
+                        title={title} />
+                    <Tab className={ tabs ? "defaultTabs" : "collapsed-tabs" } connect={connect}>
                         {tabLabels.map((label, i) => (
                             <div key={i}>
                                 <a href="/#" data-uk-switcher-item={i}>{label}</a>
@@ -66,7 +56,6 @@ const Tabs = ({
                 {children}
             </div>
         </a.div>
-        </>
     );
 }
 
--- presentation/Tab.js
+++ presentation/Tab.js
@@ -1,12 +1,9 @@
+// Tab.js
 import React from "react";
 
-const Tab = ({children, ...props}) => {
-    return (
-        <div className={`sticky-tab-container uk-subnav-pill ${props.className}`}
-                data-uk-switcher={`connect: .${props.connect}; animation: uk-animation-slide-bottom, uk-animation-slide-top-small`}>
-                {children}
-        </div>
-    )
-}
+const Tab = ({children, ...props}) =>
+    <div className={`sticky-tab-container tab-item ${props.className}`}>
+        {children}
+    </div>
 
 export default Tab;
